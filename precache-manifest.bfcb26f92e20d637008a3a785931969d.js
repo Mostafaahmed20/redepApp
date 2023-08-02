983b9155002b3ac9af5304b5cac13e18ce030f12
mostafa ahmed <42159268+Mostafaahmed20@users.noreply.github.com>
2023-08-02 23:35:54 +0300

    Updates

---
+++ precache-manifest.bfcb26f92e20d637008a3a785931969d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d62384bf633124b931ca",
+    "url": "/redepApp/static/css/main.c9dbf908.chunk.css"
+  },
+  {
+    "revision": "d62384bf633124b931ca",
+    "url": "/redepApp/static/js/main.7f8ffb82.chunk.js"
+  },
+  {
+    "revision": "99929932ead964f170c8",
+    "url": "/redepApp/static/js/runtime~main.9952b595.js"
+  },
+  {
+    "revision": "2758281c1db03ce7ac4c",
+    "url": "/redepApp/static/css/2.e40c88b9.chunk.css"
+  },
+  {
+    "revision": "2758281c1db03ce7ac4c",
+    "url": "/redepApp/static/js/2.bab181b4.chunk.js"
+  },
+  {
+    "revision": "fa29580f19f015dbcb911da76ddc83e9",
+    "url": "/redepApp/static/media/daniel-olah-2lMK4dgqwFM-unsplash.fa29580f.jpg"
+  },
+  {
+    "revision": "11b2a1cb8e0b9d7617a8c6a4adbe0dd8",
+    "url": "/redepApp/index.html"
+  }
+];
