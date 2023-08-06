382e337bb79eadb52b3f5de4386a3dc261938544
mostafa ahmed <42159268+Mostafaahmed20@users.noreply.github.com>
2023-08-06 05:40:25 +0300

    update

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -3,21 +3,21 @@ import Home from "./Home";
 import "../App.css";
 import MyNavpar from "./Navpar";
 
-const Fotter = () => {
-  return (
-    <div>
-      <h3>
-        Created By<bold style={{ color: "red" }}>🎉🎉</bold>{" "}
-      </h3>
-      <h4> Mostafa {new Date().getFullYear()}</h4>
-    </div>
-  );
-};
+// const Fotter = () => {
+//   return (
+//     <div>
+//       <h3>
+//         Created By<bold style={{ color: "red" }}>🎉🎉</bold>{" "}
+//       </h3>
+//       <h4> Mostafa {new Date().getFullYear()}</h4>
+//     </div>
+//   );
+// };
 function MainComponent() {
   return (
     <div>
       <MyNavpar />
-      <Fotter />
+      {/* <Fotter /> */}
     </div>
   );
 }
